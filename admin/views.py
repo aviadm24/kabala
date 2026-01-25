ae5cc78d811e9941cea092b5f83a9aa69210589c
aviadm24 <aviadm24@gmail.com>
2026-01-26 01:18:43 +0200

    add Receipt model to admin view

--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,6 @@
 # admin/views.py
 from sqladmin import Admin, ModelView
-from models import User
+from models import User, Receipt
 
 
 class UserAdmin(ModelView, model=User):
@@ -8,8 +8,13 @@ class UserAdmin(ModelView, model=User):
     column_searchable_list = [User.email]
     can_delete = False
 
+class ReceiptAdmin(ModelView, model=Receipt):
+    column_list = [Receipt.username, Receipt.name, Receipt.secure_url]
+    column_searchable_list = [Receipt.name, Receipt.username]
+    can_delete = False
 
 def setup_admin(app, engine):
     admin = Admin(app, engine)
     admin.add_view(UserAdmin)
+    admin.add_view(ReceiptAdmin)
     return admin
